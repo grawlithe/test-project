cd5b7044033100631087259e9c179ec079a8b224
raul.adrian <raul.adrian.altavano@sedp.ph>
2023-09-26 11:22:14 +0800

    improved response handling

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -10,59 +10,98 @@ const step = ref(1);
 <script>
   export default {
     data: () => ({
-      email:'',
-      firstName:'',
-      lastName:'',
-      type:'',
-      password:'',
-      company:'',
-      address:'',
-      city:'',
-      state:'',
-      zip:'',
-      country:'',
-      userType:''
+      dataForm: {
+        email:'',
+        firstName:'',
+        lastName:'',
+        type:'test',
+        password:'',
+        cpassword:'',
+        company:'',
+        address:'',
+        city:'',
+        state:'',
+        zip:'',
+        country:'',
+        userType:'Admin',
+      },
+      countryList: ''
     }),
+    mounted() {
+      this.getCountry();
+    },
+    computed: {
+      samePassword() {
+        return this.dataForm.password === this.dataForm.cpassword ? true : false
+      }
+    },
     methods: {
-      async alert() {
+      submitData() {
+        //console.log(this.dataForm)
+        fetch('https://renting-api.onrender.com/users/register', {
+          method: 'POST',
+          body: JSON.stringify(this.dataForm),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            'Authorization': 'Bearer 6nv9i5abfaipo2yks0vku611ut9y7x',
+          }
+        })
+        .then(response => Promise.all([response, response.json()]))
+        .then(([response, json]) => {
+            //console.log('response', response);
+            if (!response.ok || !response.status === 201) { 
+                throw Error(`Respsonse status ${response.status} (${response.statusText}): ${json.message}`);
+            }
+            console.log(json);
+            alert("Saved Successfully!");
+        })
+        .catch(exception => {
+            console.log(new Map([
+                [TypeError, "There was a problem fetching the response."],
+                [SyntaxError, "There was a problem parsing the response."],
+                [Error, exception.message]
+            ]).get(exception.constructor()));
+        });
+      },
+      async getCountry() {
         const a = await fetch(`https://restcountries.com/v3.1/all`);
         const d = await a.json();
-        console.log(d.map((m) => {  }))
+        this.countryList = d.map((d) => { return d.name.common }).sort()
       }
-    }
+    },
   }
 </script>
 
 <template>
   <div class="stepone" v-show="step==1">
-    <input type="email" name="email" v-model="email" />
     <WelcomeItem>
       <template #heading>Email</template>
       
+      <input type="email" name="email" v-model="dataForm.email" />
     </WelcomeItem>
     
     <WelcomeItem>
       <template #heading>First Name</template>
       
-      <input type="text" name="firstname" v-model="firstname" />
+      <input type="text" name="firstname" v-model="dataForm.firstName" />
     </WelcomeItem>
     
     <WelcomeItem>
       <template #heading>Last Name</template>
       
-      <input type="text" name="lastname" v-model="lastname" />
+      <input type="text" name="lastname" v-model="dataForm.lastName" />
     </WelcomeItem>
     
     <WelcomeItem>
       <template #heading>Password</template>
       
-      <input type="password" name="password" v-model="password" />
+      <input type="password" name="password" v-model="dataForm.password" />
     </WelcomeItem>
     
     <WelcomeItem>
       <template #heading>Confirm Password</template>
       
-      <input type="password" name="cpassword" v-model="cpassword" />
+      <input type="password" name="cpassword" v-model="dataForm.cpassword" />
     </WelcomeItem>
   </div>
 
@@ -71,39 +110,45 @@ const step = ref(1);
     <WelcomeItem>
       <template #heading>Company</template>
       
-      <input type="text" name="company" v-model="company" />
+      <input type="text" name="company" v-model="dataForm.company" />
     </WelcomeItem>
     
     <WelcomeItem>
       <template #heading>Address</template>
       
-      <input type="text" name="address" v-model="address" />
+      <input type="text" name="address" v-model="dataForm.address" />
     </WelcomeItem>
     
     <WelcomeItem>
       <template #heading>City</template>
       
-      <input type="text" name="city" v-model="city" />
+      <input type="text" name="city" v-model="dataForm.city" />
     </WelcomeItem>
     
     <WelcomeItem>
       <template #heading>State</template>
       
-      <input type="text" name="state" v-model="state" />
+      <input type="text" name="state" v-model="dataForm.state" />
     </WelcomeItem>
     
     <WelcomeItem>
       <template #heading>Country</template>
       
-      <input type="text" name="country" v-model="country" />
+      <select v-model="dataForm.country">
+        <option disabled value="">Please select one</option>
+        <option v-for="(item, i) in countryList" :value="item" :key="i">{{ item }}</option>
+      </select>
     </WelcomeItem>
+
   </div>
-  <div>
+  <WelcomeItem>
+
     <button @click="step--" v-if="step==2">Back</button>
     <button @click="step++" v-if="step==1">Next</button>
-    <button @click="alert" v-if="step==2">Submit</button>
-  </div>
+    <button @click="submitData" v-if="step==2" :disable="samePassword">Submit</button>
+  </WelcomeItem>
 </template>
+
 <style scoped>
 
 </style>
